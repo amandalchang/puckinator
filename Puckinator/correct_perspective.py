faaed383b244b0aeb1511ef2f65060b255e428d8
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-10-31 20:29:56 -0400

    add comments

--- Puckinator/correct_perspective.py
+++ Puckinator/correct_perspective.py
@@ -3,21 +3,19 @@ import numpy as np
 
 
 def order_points(pts):
-    # Initialize a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
+    # Initialize an array to hold the ordered coordinates of the points
+    # The order is determined as follows: [top-left, top-right, bottom-right, bottom-left]
     rect = np.zeros((4, 2), dtype="float32")
 
-    # The top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
+    # Sum the coordinates of each point; the smallest sum will be the top-left point,
+    # and the largest sum will be the bottom-right point.
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
 
-    # Compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
+    # Compute the difference between the coordinates of each point;
+    # the smallest difference will be the top-right point,
+    # and the largest difference will be the bottom-left point.
     diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
@@ -27,39 +25,44 @@ def order_points(pts):
 
 
 def main():
-    # Get the predefined dictionary
+    # Load a predefined dictionary for ArUco marker detection
     dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
-    # Create detector parameters
+    # Create an instance of DetectorParameters for configuring ArUco detection
     parameters = cv.aruco.DetectorParameters()
-    # Instantiate the ArucoDetector object
+    # Create an ArucoDetector object with the predefined dictionary and custom parameters
     detector = cv.aruco.ArucoDetector(dictionary, parameters)
 
+    # Initialize the video capture object to capture video from the default camera (camera 0)
     cap = cv.VideoCapture(0)
 
     while True:
+        # Capture a frame from the camera
         ret, frame = cap.read()
+        # Check if the frame was successfully captured
         if not ret:
             print("Failed to grab frame")
-            break
+            break  # Exit the loop if frame capture failed
 
-        # Detect markers
+        # Detect ArUco markers in the frame
         markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(frame)
 
+        # Check if any ArUco markers were detected
         if markerIds is not None:
-            # Draw marker boundaries
+            # Draw the boundaries of the detected ArUco markers on the frame
             cv.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)
 
-            if len(markerCorners) == 4:  # Ensure there are exactly 4 markers detected
+            # Proceed if exactly four ArUco markers are detected
+            if len(markerCorners) == 4:
                 # Calculate the center of each ArUco marker
                 centers = np.array([np.mean(crn[0], axis=0) for crn in markerCorners])
 
-                # Sort and order the center points
+                # Order the center points of the ArUco markers
                 sorted_corners = order_points(centers)
 
-                # Define the dimensions of your paper in pixels
-                # You might want to adjust the width and height values according to your needs
+                # Define the dimensions of the paper in pixels
                 paper_width = 1500
                 paper_height = 800
+                # Define the coordinates of the corners of the paper in the output image
                 output_pts = np.array(
                     [
                         [0, 0],
@@ -70,25 +73,30 @@ def main():
                     dtype="float32",
                 )
 
-                # Compute the perspective transform matrix
+                # Compute the perspective transform matrix to transform the perspective
+                # of the captured frame to match the dimensions of the paper
                 M = cv.getPerspectiveTransform(sorted_corners, output_pts)
 
-                # Apply the perspective transformation
+                # Apply the perspective transformation to the captured frame
                 warped = cv.warpPerspective(frame, M, (paper_width, paper_height))
 
-                # Show the result
+                # Display the result of the perspective transformation
                 cv.imshow("Perspective Transform", warped)
 
-        # Show the frame
+        # Display the original frame with the detected ArUco markers
         cv.imshow("Frame", frame)
 
+        # Wait for a key press for 1 millisecond; exit if 'Esc' is pressed
         key = cv.waitKey(1)
         if key == 27:
             break
 
+    # Release the video capture object to free resources
     cap.release()
+    # Destroy all OpenCV-created windows to free resources
     cv.destroyAllWindows()
 
 
+# Execute the main function when this script is run
 if __name__ == "__main__":
     main()
